36de2e310a68247ce5dd578ca486a3b943b46238
정아영 <p_111@naver.com>
2020-09-14 01:54:15 +0900

    issue complete at CORS

--- backend/src/main/java/com/ssafy/smaheal/controller/EmotionController.java
+++ backend/src/main/java/com/ssafy/smaheal/controller/EmotionController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/emotion")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EmotionController {
 
 	private static final String SUCCESS = "success";
