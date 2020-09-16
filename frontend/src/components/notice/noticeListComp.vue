837e151cb9f4c3301c59b6722f03262e7c4f4d13
정아영 <p_111@naver.com>
2020-09-17 01:38:10 +0900

    complete list_front, write_front

--- frontend/src/components/notice/noticeListComp.vue
+++ frontend/src/components/notice/noticeListComp.vue
@@ -1,12 +1,9 @@
 <template>
-  <v-container>
-    <v-row>
-        <v-col>
-            <v-btn text class="orange white--text">
-              공지사항
-            </v-btn>
-        </v-col>
-    </v-row>
+  <v-container class="pa-0">
+    <v-sheet class="pa-6" dark>
+      <v-icon class="mr-2">mdi-clipboard-alert-outline</v-icon>
+      공지사항
+    </v-sheet>
     <v-row>
       <v-col>
         <v-spacer/>
@@ -14,20 +11,19 @@
       <v-col>
         <v-autocomplete
         v-model="searchNotice"
-        :search-input.sync="search"
         cache-items
-        class="mx-4"
+        class="mx-4 ml-16"
         flat
         hide-no-data
         hide-details
-        label="search"
+        label="제목으로 검색됩니다. 검색어를 입력하세요."
         solo-inverted
         ></v-autocomplete>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
-        <v-simple-table>
+        <v-simple-table dark>
           <template v-slot:default>
             <thead>
               <tr>
@@ -42,24 +38,33 @@
                 <td>{{item.noticeid}}</td>
                 <td @click="goDetail(item.noticeid)">{{item.title}}</td>
                 <td>관리자</td>
-                <td>{{item.created_at}}</td>
+                <td>{{getFormatDate(item.createdAt)}}</td>
               </tr>
             </tbody>
           </template>
         </v-simple-table>
       </v-col>
     </v-row>
+    <div class="text-center">
+      <v-pagination
+      v-model="currentPage"
+      :length="totalPage"
+      :total-visible="visibleCnt"
+      color="black"
+      />
+    </div>
     <v-row>
-      <v-col>
-        <v-btn @click="goNoticeRegist()" text>
-          글 작성하기
-        </v-btn>
+      <v-col class="text-right">
+        <v-btn @click="goNoticeRegist()" outlined>
+        글 작성하기
+      </v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import moment from "moment";
 
 export default {
   name: "noticeListComp",
@@ -71,6 +76,10 @@ export default {
      items: this.propItems,
      searchNotice: "",
      search: "",
+     currentPage: 1,
+     dataPerPage: 7,
+     totalPage: 5,
+     visibleCnt: 5,
    };
   },
   methods: {
@@ -79,6 +88,9 @@ export default {
       },
       goDetail: function(noticeid) {
         this.$router.push(`/notice/detail/${noticeid}`)
+      },
+      getFormatDate: function(regtime) {
+        return moment(new Date(regtime)).format("YYYY년 MM월 DD일");
       }
   },
 };
--- frontend/src/components/notice/noticeWriteComp.vue
+++ frontend/src/components/notice/noticeWriteComp.vue
@@ -1,28 +1,48 @@
 <template>
-  <v-container>
-    <v-sheet color="orange lighten-2">공지사항 작성페이지</v-sheet>
-    <v-sheet outlined class="pa-5 mb-5">
+  <v-container class="pa-0">
+    <v-sheet class="pa-6 mt-4" dark>
+      <v-icon class="mr-2">mdi-clipboard-edit-outline</v-icon>
+      공지사항 작성페이지
+    </v-sheet>
+    <v-sheet dark class="pa-5 mb-5">
         <v-text-field
         placeholder="제목을 입력하세요."
-        color="orange"
+        color="white"
         v-model="title"
+        ref="title"
         class="headline"
+        prepend-icon="mdi-lightbulb-on"
+        clearable
+        :counter="20"
+        :rules="titleRules"
         />
         <v-textarea
         placeholder="공지사항 내용을 입력하세요."
-        color="orange"
+        color="white"
         v-model="content"
+        ref="content"
+        outlined
+        clearable
+        height="300"
+        :counter="1000"
+        :rules="contentRules"
         />
     </v-sheet>
-    <v-btn @click="noticeTempRegist()" text>
-        임시저장
-    </v-btn>
-    <v-btn @click="goBack()" text>
-        취소
-    </v-btn>
-    <v-btn @click="noticeRegist()" text>
-        등록
-    </v-btn>
+    <v-row>
+        <v-col>
+            <v-btn @click="noticeTempRegist()" outlined>
+                임시저장
+            </v-btn>
+        </v-col>
+        <v-col class="text-right">
+            <v-btn class="mr-5" @click="goBack()" outlined>
+                취소
+            </v-btn>
+            <v-btn @click="noticeRegist()" outlined>
+                등록
+            </v-btn>
+        </v-col>
+    </v-row>
 
     <v-dialog dark v-model="regist" max-width="400">
       <v-card>
@@ -52,11 +72,30 @@ export default {
        regist: false,
        alertMsg: "",
        alert: false,
+       titleRules: [
+           v => (v && v.length <= 20) || "제목은 20글자 이내만 가능합니다."
+       ],
+       contentRules: [
+           v => (v && v.length <= 1000) || "내용은 1000글자 이내만 가능합니다."
+       ],
    };
   },
   methods: {
       noticeRegist: function() {
-          this.regist = true
+          let err = true;
+          let msg = "";
+
+          !this.title && ((msg = "제목을 입력해주세요"),
+          (err = false), this.$refs.title.focus());
+          !this.content && ((msg = "내용을 입력해주세요"),
+          (err = false), this.$refs.content.focus());
+
+          if (!err) {
+              this.alertMsg = msg;
+              this.alert = true;
+          } else {
+              this.regist = true;
+          }
       },
       noticeRegistHandler: function() {
           http.post("/notice/write", {
