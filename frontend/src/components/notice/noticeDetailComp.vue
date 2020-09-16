68c162b60ca58bea58633e7f83aa90382fb44b8e
정아영 <p_111@naver.com>
2020-09-16 14:27:33 +0900

    create notice detailPage, notice WritePage

---
+++ frontend/src/components/notice/noticeDetailComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <div>
+        {{this.item.title}}
+    </div>
+    <div>
+        {{this.item.content}}
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "noticeDetailComp",
+  props: {
+      propItem: {type: Object}
+  },
+  data(){
+   return {
+       item: this.propItem,
+   };
+  },
+};
+</script>
