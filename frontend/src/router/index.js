994fee9ddc2e3b76fa38f456dcd1973eb2e088f2
정아영 <p_111@naver.com>
2020-09-16 14:26:07 +0900

    complete error at write method, view list

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,8 @@ import Home from "../views/Home.vue";
 import DonationDetail from "../views/board/DonationDetail.vue"
 import Login from "../views/account/Login.vue";
 import noticeList from "../views/notice/noticeList.vue"
+import noticeWrite from "../views/notice/noticeWrite.vue"
+import noticeDetail from "../views/notice/noticeDetail.vue"
 
 Vue.use(VueRouter);
 
@@ -28,6 +30,18 @@ const routes = [{
         path: "/notice",
         name: "noticeList",
         component: noticeList
+    },
+
+    {
+        path: "/notice/write",
+        name: "noticeWrite",
+        component: noticeWrite
+    },
+
+    {
+        path: "/notice/detail/:noticeid",
+        name: "noticeDetail",
+        component: noticeDetail
     },
 ];
 
