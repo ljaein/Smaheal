dab13cc831d0f72addc623d7a6f96731a3b14d90
정아영 <p_111@naver.com>
2020-09-17 17:17:05 +0900

    create notice_modify_front

---
+++ frontend/src/views/notice/noticeModify.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <noticeModifyComp
+    :propItems="this.items"
+    v-if="this.items.length !== 0"
+    />
+  </div>
+</template>
+
+<script>
+import noticeModifyComp from "@/components/notice/noticeModifyComp.vue";
+import http from "@/util/http-common";
+
+export default {
+  name: "noticeModify",
+  components: {
+    noticeModifyComp
+  },
+  data: function() {
+    return {
+        items: [],
+    }
+  },
+  created() {
+      http
+      .get(`/notice/find/${this.$route.params.noticeid}`)
+      .then(({data}) => {
+        this.items = data;
+      })
+  }
+};
+</script>
