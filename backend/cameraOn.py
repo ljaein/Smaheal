1905c78277f3b77b2acd3dc359f334e2acdffa1e
노휘종 <gnlwhd6498@naver.com>
2020-09-24 21:56:03 +0900

    path test2
    Former-commit-id: 1a219f015c2ed55e856c069a52cc5e7ecf3d263c

--- backend/cameraOn.py
+++ backend/cameraOn.py
@@ -15,8 +15,8 @@ def main(argv):
 
     # Face detection XML load and trained model loading
     # 경로 확인
-    face_detection = cv2.CascadeClassifier('files/haarcascade_frontalface_default.xml')
-    emotion_classifier = load_model('files/emotion_model.hdf5', compile=False)
+    face_detection = cv2.CascadeClassifier('./files/haarcascade_frontalface_default.xml')
+    emotion_classifier = load_model('./files/emotion_model.hdf5', compile=False)
     EMOTIONS = ["Angry" ,"Disgusting","Fearful", "Happy", "Sad", "Surpring", "Neutral"]
 
     # Video capture using webcam
