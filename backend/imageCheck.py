a8d59a4cd38d0c0fc9bb8f2e1f4f08e882ae2a5b
노휘종 <gnlwhd6498@naver.com>
2020-09-25 09:32:05 +0000

    Update backend/imageCheck.py
    Former-commit-id: c9689a1f968b7a3119455f0261aa6b7831bf717b

--- backend/imageCheck.py
+++ backend/imageCheck.py
@@ -25,40 +25,40 @@ def main(argv):
     color = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
 
     # 경로 확인
-    face_detection = cv2.CascadeClassifier('/var/lib/jenkins/workspace/maven-test/backend/files/haarcascade_frontalface_default.xml')
-    emotion_classifier = load_model('/var/lib/jenkins/workspace/maven-test/backend/files/emotion_model.hdf5', compile=False)
-    EMOTIONS = ["Angry" ,"Disgusting","Fearful", "Happy", "Sad", "Surpring", "Neutral"]
+    # face_detection = cv2.CascadeClassifier('/var/lib/jenkins/workspace/maven-test/backend/files/haarcascade_frontalface_default.xml')
+    # emotion_classifier = load_model('/var/lib/jenkins/workspace/maven-test/backend/files/emotion_model.hdf5', compile=False)
+    # EMOTIONS = ["Angry" ,"Disgusting","Fearful", "Happy", "Sad", "Surpring", "Neutral"]
 
-    # Convert color to gray scale
-    gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    # # Convert color to gray scale
+    # gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 
-    faces = face_detection.detectMultiScale(gray,
-                                                    scaleFactor=1.1,
-                                                    minNeighbors=5,
-                                                    minSize=(30,30))
+    # faces = face_detection.detectMultiScale(gray,
+    #                                                 scaleFactor=1.1,
+    #                                                 minNeighbors=5,
+    #                                                 minSize=(30,30))
 
 
-    # Create empty image
-    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    # # Create empty image
+    # canvas = np.zeros((250, 300, 3), dtype="uint8")
 
-    # For the largest image
-    face = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-    (fX, fY, fW, fH) = face
-    # Resize the image to 48x48 for neural network
-    roi = gray[fY:fY + fH, fX:fX + fW]
-    roi = cv2.resize(roi, (48, 48))
-    roi = roi.astype("float") / 255.0
-    roi = img_to_array(roi)
-    roi = np.expand_dims(roi, axis=0)
+    # # For the largest image
+    # face = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+    # (fX, fY, fW, fH) = face
+    # # Resize the image to 48x48 for neural network
+    # roi = gray[fY:fY + fH, fX:fX + fW]
+    # roi = cv2.resize(roi, (48, 48))
+    # roi = roi.astype("float") / 255.0
+    # roi = img_to_array(roi)
+    # roi = np.expand_dims(roi, axis=0)
 
-    # Emotion predict
-    preds = emotion_classifier.predict(roi)[0]
-    emotion_probability = np.max(preds)
-    label = EMOTIONS[preds.argmax()]
+    # # Emotion predict
+    # preds = emotion_classifier.predict(roi)[0]
+    # emotion_probability = np.max(preds)
+    # label = EMOTIONS[preds.argmax()]
 
-    # print(str(round(emotion_probability * 100)))
-    print(str(format(round(preds[3] * 100),".0f")))
-    print(label)
+    # # print(str(round(emotion_probability * 100)))
+    # print(str(format(round(preds[3] * 100),".0f")))
+    # print(label)
 
     now = int(round(time.time() * 1000))
     filename = str(now) + '.png'
