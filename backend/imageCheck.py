9e18f3f1ae13189ad86f25aa21a8f52070083a8b
노휘종 <gnlwhd6498@naver.com>
2020-09-25 09:12:33 +0000

    Update backend/imageCheck.py
    Former-commit-id: 300e43c5c0bd0f176b53a45294f69dc57d370067

--- backend/imageCheck.py
+++ backend/imageCheck.py
@@ -8,7 +8,7 @@ import base64
 def main(argv):
     # 경로 정해주기
     print(os.path.realpath(__file__))
-    r = open('./../frontend/public/textFiles/' + '1601024102786', mode='rt', encoding='utf-8')
+    r = open('/var/lib/jenkins/workspace/maven-test/frontend/public/textFiles/' + '1601024102786', mode='rt', encoding='utf-8')
     raw = ""
     for line in r:
         raw += line
@@ -25,8 +25,8 @@ def main(argv):
     color = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
 
     # 경로 확인
-    face_detection = cv2.CascadeClassifier('./files/haarcascade_frontalface_default.xml')
-    emotion_classifier = load_model('./files/emotion_model.hdf5', compile=False)
+    face_detection = cv2.CascadeClassifier('/var/lib/jenkins/workspace/maven-test/backend/files/haarcascade_frontalface_default.xml')
+    emotion_classifier = load_model('/var/lib/jenkins/workspace/maven-test/backend/files/emotion_model.hdf5', compile=False)
     EMOTIONS = ["Angry" ,"Disgusting","Fearful", "Happy", "Sad", "Surpring", "Neutral"]
 
     # Convert color to gray scale
@@ -70,7 +70,7 @@ def main(argv):
     # cv2.imwrite("C:/image/" + filename, frame)
 
     # 경로 확인
-    cv2.imwrite("./../frontend/public/images/" + filename, color)
+    cv2.imwrite("/var/lib/jenkins/workspace/maven-test/frontend/public/images/" + filename, color)
     print(filename)
 
     # cv2.imshow('Sample', color)
