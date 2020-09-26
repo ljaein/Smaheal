36365090c67d860eb9ea5eb2d4e7f03212457e63
정아영 <p_111@naver.com>
2020-09-26 12:51:16 +0900

    create imageslide_template 1
    Former-commit-id: d064b3e93e05746f0ee41680859ce0bce1609ccd

---
+++ frontend/src/views/template/SlideImage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <SlideImageComp/>
+    </div>
+</template>
+
+<script>
+import SlideImageComp from "@/components/template/SlideImageComp.vue"
+
+export default {
+    name: "SlideImage",
+    components: {
+        SlideImageComp
+    },
+    data: function() {
+        return {
+
+        }
+    }
+}
+</script>
