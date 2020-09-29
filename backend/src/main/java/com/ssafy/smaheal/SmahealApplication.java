516c1098d59d6c53cba0bdc9db4218f1761c36a0
정아영 <p_111@naver.com>
2020-09-29 17:44:52 +0900

    complete setting in serialNumber
    Former-commit-id: 69959d8e45d248ec579646ced57196e49a891386

--- backend/src/main/java/com/ssafy/smaheal/SmahealApplication.java
+++ backend/src/main/java/com/ssafy/smaheal/SmahealApplication.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,18 +14,18 @@ import com.ssafy.smaheal.help.ApplicationContextProvider;
 import com.ssafy.smaheal.model.Donation;
 import com.ssafy.smaheal.repository.DonationRepository;
 
-
 @SpringBootApplication
 public class SmahealApplication {
-	
+
 	static Calendar date;
 	static String time;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmahealApplication.class, args);
-		
+
 		// 24시마다 마감된 게시물을 가져오고 일련번호를 부여하기 위함
-		DonationRepository donationRepository = ApplicationContextProvider.getApplicationContext().getBean(DonationRepository.class);
+		DonationRepository donationRepository = ApplicationContextProvider.getApplicationContext()
+				.getBean(DonationRepository.class);
 		date = Calendar.getInstance();
 		Timer timer = new Timer();
 		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
@@ -37,22 +38,25 @@ public class SmahealApplication {
 				date = Calendar.getInstance();
 				time = format.format(date.getTime());
 				String today = todayF.format(date.getTime());
-				
-				if (time.equals("16:28")) {
+
+				if (time.equals("00:00")) {
 					List<Donation> list = donationRepository.findByApprovalAndTempOrderByCreatedateDesc(1, 0);
-					System.out.println(list.size());
-//					for (Donation donation : list) {
-//						if (!donation.getEdate().equals(today)) {
-//						donation.setApproval(2);
-//						donation.setTemplate("임시 일련번호");
-//							System.out.println("hi");
-//						}
-//					}
+
+					for (Donation donation : list) {
+						if (donation.getEdate().equals(today)) {
+
+							donation.setApproval(2);
+							String uid = UUID.randomUUID().toString();
+							donation.setTemplate(uid);
+							donationRepository.save(donation);
+
+						}
+					}
 				}
 			}
 		};
 
 		timer.scheduleAtFixedRate(tt, 0, 5 * 6000);
 	}
-	
+
 }
