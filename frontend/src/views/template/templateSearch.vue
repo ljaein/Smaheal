c974fc5dbd7a56b7339c416f8f4ec0642d0f275f
정아영 <p_111@naver.com>
2020-10-01 01:56:56 +0900

    test complete getting smile pictures
    Former-commit-id: c85743edc963213aacfb9afe92053cadbb784e16

--- frontend/src/views/template/templateSearch.vue
+++ frontend/src/views/template/templateSearch.vue
@@ -1,21 +1,35 @@
 <template>
     <div>
-      <searchComp/>
+      {{this.totalCnt}}
     </div>
 </template>
 
 <script>
-import searchComp from "@/components/template/searchComp.vue"
+// import searchComp from "@/components/template/searchComp.vue";
+import http from "@/util/http-common.js";
 
 export default {
     name: "templateSearch",
-    components: {
-        searchComp
-    },
     data: function() {
         return {
-            
+            keyword: "",
+            items: [],
+            totalCnt: 0,
         }
+    },
+    created() {
+        this.keyword = this.$route.query.template;
+
+        http
+        .get(`/template/search/${this.keyword}`)
+        .then(({data}) => {
+            this.items = data;
+
+            http.get(`/template/totalCount/${this.keyword}`)
+            .then(({data}) => {
+                this.totalCnt = data;
+            })
+        })
     }
 }
 </script>
