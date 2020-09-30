9e87a6ddd3926dc0a551e4e7e1a9c5bbcdc51c6e
정아영 <p_111@naver.com>
2020-10-01 01:45:58 +0900

    update front at template_search
    Former-commit-id: cf61a82012912d1dc39e617be75b1f7c19bd1b62

--- frontend/src/components/template/searchComp.vue
+++ frontend/src/components/template/searchComp.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
-      <v-col>
-        <v-form action="/template/search">
-          <v-text-field
-            placeholder="일련번호를 입력해주세요."
-            outlined
-            color="#356859"
-            clearable
-            name="template"
-            class="pa-3"
-          />
-        </v-form>
-      </v-col>
-    </div>
+  <v-container class="pa-0" fluid>
+    <v-col class="pa-0">
+      <v-img :src="require(`@/assets/main-smile.jpg`)" height="653">
+        <div style="padding-top: 25px; width: 75%; margin: 150px auto;">
+          <v-col style="width: 60%; margin: 0 auto;">
+            <v-form action="/template/search">
+              <v-text-field
+                placeholder="일련번호를 입력하세요. 기부된 사진을 검색합니다."
+                outlined
+                color="#356859"
+                clearable
+                name="template"
+                class="pa-0"
+                background-color="white"
+              />
+            </v-form>
+          </v-col>
+        </div>
+      </v-img>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
