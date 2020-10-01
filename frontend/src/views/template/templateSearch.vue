eafa7e8d93c9465e9f40625dd07e179cf0bf0044
정아영 <p_111@naver.com>
2020-10-01 22:18:55 +0900

    complete at template version 1
    Former-commit-id: 3702219b96f446ccc4f869db0939e63fe944a569

--- frontend/src/views/template/templateSearch.vue
+++ frontend/src/views/template/templateSearch.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-      {{this.totalCnt}}
-    </div>
+    <v-container>
+      <v-row>
+          <v-col>
+              총 {{this.totalCnt}}개의 사진에 대한 템플릿입니다.
+          </v-col>
+      </v-row>
+      <!-- <v-row v-for="(item, index) in items" :key="index">
+          <v-col>
+            <v-img :src="require(`../../../public/images/${item.photo}`)"/>
+          </v-col>
+      </v-row> -->
+      <v-row>
+          <v-col>
+            <fallImageComp
+            :propsItem="items"
+            v-if="items.length !== 0"
+            />
+          </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
-// import searchComp from "@/components/template/searchComp.vue";
+import fallImageComp from "@/components/template/fallImageComp.vue";
 import http from "@/util/http-common.js";
 
 export default {
     name: "templateSearch",
+    components: {
+        fallImageComp,
+    },
     data: function() {
         return {
             keyword: "",
