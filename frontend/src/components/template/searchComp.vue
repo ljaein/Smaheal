3b8181c5b92ea2d8c537afea01231e92ca7ca15b
정아영 <p_111@naver.com>
2020-10-01 13:51:47 +0900

    modify front about template_search
    Former-commit-id: fdac1f62147cb0fcf7ab6855a42a81b3438df895

--- frontend/src/components/template/searchComp.vue
+++ frontend/src/components/template/searchComp.vue
@@ -3,9 +3,10 @@
     <v-col class="pa-0">
       <v-img :src="require(`@/assets/main-smile.jpg`)" height="653">
         <div style="padding-top: 25px; width: 75%; margin: 150px auto;">
-          <v-col style="width: 60%; margin: 0 auto;">
+          <v-col style="width: 50%; margin: 0 auto;">
             <v-form action="/template/search">
-              <v-text-field
+              <v-row>
+                <v-text-field
                 placeholder="일련번호를 입력하세요. 기부된 사진을 검색합니다."
                 outlined
                 color="#356859"
@@ -13,7 +14,16 @@
                 name="template"
                 class="pa-0"
                 background-color="white"
-              />
+                v-model="keyword"
+                ref="search"
+                />
+                <v-btn color="#356859" dark @click="searchTemplate()"
+                large
+                class="ma-1"
+                >
+                  찾기
+                </v-btn>
+              </v-row>
             </v-form>
           </v-col>
         </div>
@@ -27,8 +37,17 @@ export default {
     name: "searchComp",
     data: function() {
         return {
-
+          keyword: "",
         }
-    }
+    },
+    methods: {
+      searchTemplate: function() {
+        if (this.keyword == "") {
+          this.$refs.search.focus();
+        } else {
+          this.$router.push(`/template/search?template=${this.keyword}`);
+        }
+      }
+    },
 }
 </script>
