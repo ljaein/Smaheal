f3c44d8994b0840f44a8a5fa84292fd77988f5ba
임도현 <gigjf@naver.com>
2020-10-04 22:53:45 +0900

    flipBook
    Former-commit-id: 2d2ade01e6dc7054014031965f7a879eeeb9dcd7

---
+++ frontend/src/components/test/FlipBookComp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div align="center">
+    <br/>
+    <flipbook class="flipbook" :pages="pages" v-slot="flipbook" :zooms="[1]">
+      <v-btn @click="flipbook.flipLeft">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn @click="flipbook.flipRight">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </flipbook>
+  </div>
+</template>
+
+<script>
+import flipbook from "flipbook-vue";
+
+export default {
+  name: "FlipBookComp",
+  components: { flipbook },
+  data() {
+    return {
+      pages: [
+        null,
+        "images/1600847574137.png",
+        "images/1601362225543.png",
+        "images/1601366107975.png",
+        "images/1600847574137.png",
+        "images/1601362225543.png",
+        "images/1601366107975.png"
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.flipbook {
+  width: 90vw;
+  height: 60vh;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -27,6 +27,7 @@ import templateSearch from "../views/template/templateSearch.vue"
 import Award from "../views/smile/Award.vue"
 import TempDetailComp from "../components/account/TempDetailComp.vue"
 import ebookComp from "../components/template/ebookComp.vue"
+import FlipBook from "../views/test/FlipBook.vue"
 
 Vue.use(VueRouter);
 
@@ -169,6 +170,11 @@ const routes = [{
         path: "/ebook",
         name: "ebookComp",
         component: ebookComp
+    },
+    {
+        path: "/flipBook",
+        name: "FlipBook",
+        component: FlipBook
     },
 ];
 
