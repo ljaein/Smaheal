e597c74d4c20541f03159d8739443e5f5b6efb80
노휘종 <gnlwhd6498@naver.com>
2020-10-04 02:23:53 +0000

    Update frontend/src/components/template/ebookComp.vue
    Former-commit-id: 7e62d2546c0b8a7fdd13c8041dd1e8e969216559

--- frontend/src/components/template/ebookComp.vue
+++ frontend/src/components/template/ebookComp.vue
@@ -1,28 +1,13 @@
 <template>
-    <v-container>
-        <flipbook class="flipbook" :pages="['array', 'of', 'image', 'URLs']"/>
-    </v-container>
+    <div></div>
 </template>
 
 <script>
-import Flipbook from 'flipbook-vue'
 
 export default {
-    name: "ebookComp",
-    components: {
-        Flipbook
-    },
-    data: function() {
-        return {
-            
-        }
-    },
+    
 }
 </script>
 
 <style>
-.flipbook {
-  width: 90vw;
-  height: 90vh;
-}
 </style>
