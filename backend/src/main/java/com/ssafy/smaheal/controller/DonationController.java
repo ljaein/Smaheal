87a6347486caffd85c3c3800af05fc28ae98b418
정아영 <p_111@naver.com>
2020-10-07 22:56:59 +0900

    update close list function
    Former-commit-id: 13af35fe3d33d7abec79f4a08251b1e153093998

--- backend/src/main/java/com/ssafy/smaheal/controller/DonationController.java
+++ backend/src/main/java/com/ssafy/smaheal/controller/DonationController.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import com.ssafy.smaheal.model.Donation;
 import com.ssafy.smaheal.repository.DonationRepository;
@@ -319,6 +320,26 @@ public class DonationController {
         } catch (Exception e) {
     		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     	}
+    }
+    
+    @PutMapping("/template/{donationid}")
+    @ApiOperation(value = "게시물 마감 후 템플릿 부여")
+    public Object endList(@PathVariable Long donationid) throws SQLException, IOException {
+        try {
+            Donation donation = donationRepository.findByDonationid(donationid);
+            if (donation != null) {
+                donation.setApproval(2);
+                String uid = UUID.randomUUID().toString();
+				donation.setTemplate(uid);
+                donationRepository.save(donation);
+                return new ResponseEntity<>(donation, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(donation, HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
